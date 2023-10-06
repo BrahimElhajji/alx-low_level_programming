8dde9d7c138456fa6d28c00644b754f63d8390fd
BrahimElhajji <elhajji080898@gmail.com>
2023-10-05 20:04:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-int main (void)
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
 {
-	puts ("Programming is like building a multilingual puzzle\n");
+
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
