76c491fef7c433ae7157cd9e2b4a1a79a64daecb
BrahimElhajji <elhajji080898@gmail.com>
2023-10-06 12:58:33 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha != 'z')
+	{
+	putchar(alpha);
+	alpha++;
+	}
+	return (0);
+}
