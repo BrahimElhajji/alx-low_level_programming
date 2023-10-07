db6fe3b74aa37f92c289231150fab1c4acbe0fdc
BrahimElhajji <elhajji080898@gmail.com>
2023-10-07 11:27:37 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n, n1;
+
+	for (n = 0; n <= 99; n++)
+	{
+		for (n1 = 0; n1 <= 99; n1++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			if (n != 99 || n1 != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
