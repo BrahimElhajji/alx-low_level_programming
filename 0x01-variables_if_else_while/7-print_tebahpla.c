054371022b90b883d41be3d54d6f24a2e89da0f9
BrahimElhajji <elhajji080898@gmail.com>
2023-10-06 18:52:00 -0700

    Reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char aphla = 'z';
+
+	while (aphla >= 'a')
+	{
+	putchar(aphla);
+	aphla--;
+	}
+	putchar('\n');
+	return (0);
+}
