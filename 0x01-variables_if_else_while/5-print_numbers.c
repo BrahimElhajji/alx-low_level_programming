2736886d188b8b83b8e21436ab35aeb3ffcfd0a4
BrahimElhajji <elhajji080898@gmail.com>
2023-10-06 18:14:02 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while (n <= 10)
+	{
+	printf("%d\n", n);
+	n++;
+	}
+	return (0);
+}
