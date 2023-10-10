ddcf8b49ffc881c9ac19da4bdfa5717c940a42b5
BrahimElhajji <elhajji080898@gmail.com>
2023-10-10 16:19:50 -0700

    _putchar and the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entery point
+ *
+ * _putchar - is the putchar prototype
+ *
+ * Return: Always )
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
