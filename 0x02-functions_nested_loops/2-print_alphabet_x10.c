116b922ecca205e461134c604dfabb2579cdd49f
BrahimElhajji <elhajji080898@gmail.com>
2023-10-10 17:38:10 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - the function to print 10 lines of alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int ln, ch;
+
+	for (ln = 0; ln <= 10; ln++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+	}
+	_putchar('\n');
+}
