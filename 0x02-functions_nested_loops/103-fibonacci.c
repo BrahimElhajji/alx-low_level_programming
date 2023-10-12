c05f4e8034b70f9a40e27e6ee25f8bba6040676b
BrahimElhajji <elhajji080898@gmail.com>
2023-10-11 20:22:20 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - Entery point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long f = 0, f1 = 1, s;
+	float ts;
+
+		while (1)
+		{
+			s = f = f1;
+
+			if (s > 4000000)
+				break;
+
+			if ((s % 2) == 0)
+				ts += s;
+			f = f1;
+			f1 = s;
+		}
+	printf("%.0f\n", ts);
+	return (0);
+}
