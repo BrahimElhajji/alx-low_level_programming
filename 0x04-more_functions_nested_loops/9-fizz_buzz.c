a7069e6081e13d01690eb619343ef07bb98b6bf9
BrahimElhajji <elhajji080898@gmail.com>
2023-10-12 20:08:02 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,16 @@ int main(void)
 
 	for (n = 1; n <= 100; n++)
 	{
-		if ((n == 3) && (n % 3 == 0))
+		if (n % 3 == 0)
 			printf("Fizz");
-		else if ((n == 5) && (n % 5 == 0))
+		else if (n % 5 == 0)
 			printf("buzz");
-		else if ((n == 3 * 5) && (n % 3 == 0 && n % 5 == 0))
+		else if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz");
+		else
+			printf("%d", n);
+		printf(" ");
 	}
+	printf("\n");
 	return (0);
 }
