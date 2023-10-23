7896360eb4c96db9f49c68ad4c774235836afb47
BrahimElhajji <elhajji080898@gmail.com>
2023-10-23 09:19:02 -0700

    strchr a function that locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,9 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; s[i] >= '\0'; i++)
-	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			return (s);
-		s++;
+			return (s + i);
 	}
 	return (0);
 }
