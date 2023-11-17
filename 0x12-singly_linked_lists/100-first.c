80af186e787c196dd0ade2006de8839ee7ef41d9
BrahimElhajji <elhajji080898@gmail.com>
2023-11-16 20:50:23 -0800

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * main - function that prints a string before the main function is executed
+ */
+
+void bef_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
