84955213786fb3736673b3e7a31576e84b03503b
BrahimElhajji <elhajji080898@gmail.com>
2024-01-15 18:26:55 -0800

    task 2 advanced

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int add(int num1, int num2)
 {
-    return (num1 + num2);
+	return (num1 + num2);
 }
 
 int sub(int num1, int num2){
-    return (num1 - num2);
+	return (num1 - num2);
 }
 
 int mul(int num1, int num2)
 {
-    return (num1 * num2);
+	return (num1 * num2);
 }
 
-int divv(int num1, int num2)
+int div(int num1, int num2)
 {
-    return (num1 / num2);
+	return (num1 / num2);
 }
 
 int mod(int num1, int num2)
 {
-    return (num1 % num2);
+	return (num1 % num2);
 }
