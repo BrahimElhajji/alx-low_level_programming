6d8fd1047862c9aad79cb283a6dd299b2b2141b9
BrahimElhajji <elhajji080898@gmail.com>
2024-02-02 08:57:48 -0800

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,31 +10,42 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long int index, i;
+	char *v_copy;
 	hash_node_t *node;
 
 	index = key_index((const unsigned char *)key, ht->size);
-
+	v_copy = strdup(value);
+	if (v_copy == NULL)
+		return (0);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = v_copy;
+			return (1);
+		}
+	}
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
-		return (0);
-
-	node->key = strdup(key);
-	node->value = strdup(value);
-
-	if (node->key == NULL || node->value == NULL)
 	{
-		free(node->key);
-		free(node->value);
-		free(node);
+		free(v_copy);
 		return (0);
 	}
-
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		free(v_copy);
+		return (0);
+	}
+	node->value = v_copy;
 	node->next = ht->array[index];
 	ht->array[index] = node;
-
 	return (1);
 }
+
