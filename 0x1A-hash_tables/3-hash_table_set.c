07bbfd280261d6c1d889195829d38e8979e9b258
BrahimElhajji <elhajji080898@gmail.com>
2024-02-02 08:44:30 -0800

    function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 
 	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+		free(node->value);
+		free(node);
 		return (0);
+	}
 
 	node->next = ht->array[index];
 	ht->array[index] = node;
